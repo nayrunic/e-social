fd68a86915770ce01fc654f21dfd54de82294427
Camilo Perez <jcvpf@hotmail.com>
2024-06-29 12:37:17 +1000

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: "https://e-social.me",
   integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify(),
 });
