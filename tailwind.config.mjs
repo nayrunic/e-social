b9a93beed1741fd2a54982efee951d1ae0f227d0
Camilo Perez <jcvpf@hotmail.com>
2024-06-30 15:09:50 +1000

    fix bg images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-import {resolve} from 'path';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			backgroundImage: {
-				'home': resolve('src/assets/home2.jpg'),
-				'policy': resolve('src/assets/policy.jpg')
-			}
 		},
 	},
 	plugins: [],
