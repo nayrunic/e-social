fa77333264511054c411db26fc282acc5320470d
Camilo Perez <jcvpf@hotmail.com>
2024-06-30 13:22:05 +1000

    fix bg path tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import {resolve} from 'path';
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			backgroundImage: {
-				'home': "url('src/assets/home2.jpg')",
-				'policy': "url('src/assets/policy.jpg')"
+				'home': resolve('src/assets/home2.jpg'),
+				'policy': resolve('src/assets/policy.jpg')
 			}
 		},
 	},
