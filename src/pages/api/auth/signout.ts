1872b1e1c1bd702053ba545cf483a93ce9a713c8
Camilo Perez <jcvpf@hotmail.com>
2025-02-10 20:56:48 +1000

    fix signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,11 +1,7 @@
-import { setProgress, setStimuliLeft, setUser } from "@/store/store";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
-  setUser(null);
-  setStimuliLeft([]);
-  setProgress(0);
   return redirect("/");
 };
