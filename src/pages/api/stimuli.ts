c20dad330343bd62455334639ef94d67cf8f0965
Camilo Perez <jcvpf@hotmail.com>
2025-02-10 21:06:49 +1000

    fix imports

--- src/pages/api/stimuli.ts
+++ src/pages/api/stimuli.ts
@@ -1,6 +1,5 @@
 import type { APIRoute } from "astro";
 import { supabase } from "@/lib/supabase";
-import { user } from "@/store/store";
 
 export const POST: APIRoute = async ({ request, redirect }) => { 
 
