9ef6db093a2a9f40716822d5a59176e768337e38
Camilo Perez <jcvpf@hotmail.com>
2025-02-11 22:06:01 +1000

    fix survey layout?

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./src/components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			keyframes: {
